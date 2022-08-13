0fcee452c062d928acca280f2551377625616d49
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-08-13 17:46:57 +0300

    Update index.js

--- client/app/screens/Home/index.js
+++ client/app/screens/Home/index.js
@@ -1,14 +1,22 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+import ProfilHeader from '../../component/ProfilHeader';
+import SearchBox from '../../component/SearchBox';
+import { colors } from '../../config';
+import CategoriesHorizontal from '../../component/CategoriesHorizontal';
 
 export class index extends Component {
   render() {
     return (
-      <View>
-        <Text>index</Text>
+      <View style={{backgroundColor:colors.white,flex:1}}>
+        <ProfilHeader />
+        <View style={{paddingHorizontal: '6%'}}>
+          <SearchBox />
+          <CategoriesHorizontal />
+        </View>
       </View>
-    )
+    );
   }
 }
 
-export default index
+export default index;
