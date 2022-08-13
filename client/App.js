2a495277e458c1a1f4835d280317d7318e8a6e15
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-08-13 13:52:45 +0300

    düzeltme

--- client/App.js
+++ client/App.js
@@ -7,12 +7,12 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {store} from './app/redux/store';
 import {Provider} from 'react-redux';
 
-
-
 import {colors} from './app/config';
 
-import Home from './app/screens/Home'
-
+import Home from './app/screens/Home';
+import Splash from './app/screens/Welcome/Splash';
+import Splash1 from './app/screens/Welcome/Splash1';
+import Login from './app/screens/Welcome/Login';
 export const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -20,12 +20,12 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}}>
-       
-          <Stack.Screen name="Register" component={Home} />
-  
+          <Stack.Screen name="Splash" component={Splash} />
+          <Stack.Screen name="Splash1" component={Splash1} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
   );
 }
-
