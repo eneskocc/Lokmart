6ecc82ce1c014da4b91dc8f47f81c9962dcdd00f
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-08-18 16:41:18 +0300

    Update App.js

--- client/App.js
+++ client/App.js
@@ -15,6 +15,9 @@ import Splash1 from './app/screens/Welcome/Splash1';
 import Login from './app/screens/Welcome/Login';
 import Register from './app/screens/Welcome/Register';
 import Categories from './app/screens/Home/Categories';
+import Search from './app/screens/Home/Search';
+import Filter from './app/screens/Home/Filter';
+import Products from './app/screens/Home/Products';
 export const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -28,6 +31,9 @@ export default function App() {
           <Stack.Screen name="Register" component={Register} />
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Categories" component={Categories} />
+          <Stack.Screen name="Search" component={Search} />
+          <Stack.Screen name="Filter" component={Filter} />
+          <Stack.Screen name="Products" component={Products} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
