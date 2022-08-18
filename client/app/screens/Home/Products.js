90b4f32f0def2d946944ce002b7854de78d77197
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-08-18 16:42:59 +0300

    Create Products.js

---
+++ client/app/screens/Home/Products.js
@@ -0,0 +1,57 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+import Header from '../../component/Header';
+import {colors} from '../../config';
+import ProductCard from '../../component/ProductCard';
+import SearchBox from '../../component/SearchBox';
+
+export class Products extends Component {
+  render() {
+    return (
+      <View>
+        <Header txt={this.props.route.params.header} />
+        <View style={{backgroundColor: colors.white, paddingHorizontal: '6%'}}>
+          <SearchBox />
+          <ProductCard
+            header={'Fruits'}
+            img={require('../../assets/images/product/placeholder1.png')}
+            ratings={'97 Ratings'}
+            price={'15$'}
+          />
+          <ProductCard
+            header={'Fruits'}
+            img={require('../../assets/images/ic/ic_lemon.png')}
+            ratings={'97 Ratings'}
+            price={'15$'}
+          />
+          <ProductCard
+            header={'Fruits'}
+            img={require('../../assets/images/ic/ic_lemon.png')}
+            ratings={'97 Ratings'}
+            price={'15$'}
+          />
+          <ProductCard
+            header={'Fruits'}
+            img={require('../../assets/images/product/placeholder1.png')}
+            ratings={'97 Ratings'}
+            price={'15$'}
+          />
+          <ProductCard
+            header={'Fruits'}
+            img={require('../../assets/images/ic/ic_lemon.png')}
+            ratings={'97 Ratings'}
+            price={'15$'}
+          />
+          <ProductCard
+            header={'Fruits'}
+            img={require('../../assets/images/ic/ic_lemon.png')}
+            ratings={'97 Ratings'}
+            price={'15$'}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+export default Products;
